ec76d367b8c957d4ef05796efbc1f33272294d78
Ken Cheng <kenfcheng@gmail.com>
2020-09-10 16:03:32 -0400

    nodemon installed and async function created in connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -3,8 +3,11 @@ const mongoose = require("mongoose");
 const URI =
   "mongodb+srv://dbUser:dbUser@cluster0.qw6oe.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
-const connectDB = async () => {
-  await mongoose.connect(URI);
+const connectDB = async function () {
+  await mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   console.log("db connected !");
 };
 
