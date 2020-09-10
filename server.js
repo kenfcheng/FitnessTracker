825cb077d61d0cf8594862edd207a7aa84b6c69e
Ken Cheng <kenfcheng@gmail.com>
2020-09-10 11:29:40 -0400

    apiRoutes and htmlRoutes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -8,14 +8,17 @@ const app = express();
 // process.env.PORT lets the port be set by Heroku
 const PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(morgan("dev"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
 });
+
+require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
