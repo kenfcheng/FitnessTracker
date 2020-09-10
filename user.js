cb1156f7d2ce314ecd33abcf5541402fbf17a278
Ken Cheng <kenfcheng@gmail.com>
2020-09-10 16:44:38 -0400

    user.js in DB created. user.js in API created

---
+++ user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('../DB/user');
+const route = express.route();
+
+route.post('/',(req, res) => {
+    const{}
+})
